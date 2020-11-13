b5f40fd46587921e3ceac238b40d5bac47faed15
Pranta78 <48055763+Pranta78@users.noreply.github.com>
2020-11-13 13:40:19 +0600

    Message module redesigned

--- Twitter/message_view.py
+++ Twitter/message_view.py
@@ -10,20 +10,42 @@ def message(request):
     user_id = request.session.get("user_id", None)
     username = request.session.get("username", None)
 
-    # This will list all users, and hyperlink to the inbox and chat history to all users
-
+    # This will list all users, and unseen message count
+    # and hyperlink to the inbox and chat history to all users
     with connection.cursor() as cursor:
-        cursor.execute("SELECT ACCOUNTNAME FROM ACCOUNT;")
-        row = cursor.fetchall()
-        print("Printing User names...")
-        print(row)
-        userlist = [col[0] for col in row]
-        print("Printing User names pretty print...")
-        print(userlist)
+        cursor.execute("SELECT ID, ACCOUNTNAME FROM ACCOUNT WHERE ID <> :user_id;", {'user_id': user_id})
+        receiver_list = dictfetchall(cursor)
+        print("Printing Receiver list pretty print...")
+        print(receiver_list)
+
+        receiver_data = []
+
+        # gets the number of unseen messages for each receiver (users other than the logged in user)
+        for receiver in receiver_list:
+            receiver_id = receiver['ID']
+
+            cursor.execute('''SELECT COUNT(M.ID)
+                             FROM ACCOUNT_SENDS_MESSAGE ASM JOIN ACCOUNT_RECEIVES_MESSAGE ARM
+                             ON(ASM.MESSAGE_ID = ARM.MESSAGE_ID)
+                             JOIN MESSAGE M
+                             ON(ARM.MESSAGE_ID = M.ID)
+                             WHERE
+                             (ARM.ACCOUNT_ID=:user_id AND ASM.ACCOUNT_ID=:receiver_id)
+                             AND M.SEEN IS NULL;''', {'receiver_id': receiver_id, 'user_id': user_id})
+
+            UNSEEN_MSG_COUNT = cursor.fetchone()[0]
+
+            receiver_data.append({
+                'NAME': receiver['ACCOUNTNAME'],
+                'UNSEEN_MSG_COUNT': UNSEEN_MSG_COUNT
+            })
+
+        # receiver_data = dictfetchall(cursor)
+        print(receiver_data)
 
     context = {"user_id": user_id,
                "username": username,
-               "userlist": userlist,
+               "receiver_data": receiver_data,
                "messages_is_active": True}
 
     template_name = "message.html"
@@ -47,6 +69,23 @@ def inbox(request, receiver):
         cursor.execute(f"SELECT ID FROM ACCOUNT WHERE ACCOUNTNAME='{receiver}';")
         receiver_id = cursor.fetchone()[0]
         print(receiver_id)
+
+        # Mark all messages sent from the receiver as seen
+        import datetime
+        current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cursor.execute('''UPDATE MESSAGE
+                          SET SEEN = :current_time
+                          WHERE ID IN 
+                          (SELECT M.ID
+                          FROM ACCOUNT_SENDS_MESSAGE ASM JOIN ACCOUNT_RECEIVES_MESSAGE ARM
+                          ON(ASM.MESSAGE_ID = ARM.MESSAGE_ID)
+                          JOIN MESSAGE M
+                          ON(ARM.MESSAGE_ID = M.ID)
+                          WHERE ASM.ACCOUNT_ID =:receiver_id AND ARM.ACCOUNT_ID =:user_id
+                          AND M.SEEN IS NULL);''',
+                       {'receiver_id': receiver_id, 'user_id': user_id, 'current_time': current_time})
+        connection.commit()
+
         cursor.execute(f'''SELECT M.TEXT, M.MEDIA, M.TIMESTAMP, M.SEEN,
                         (SELECT ACCOUNTNAME FROM ACCOUNT A WHERE A.ID=ASM.ACCOUNT_ID) NAME,
                         (SELECT PROFILE_PHOTO FROM ACCOUNT A WHERE A.ID=ASM.ACCOUNT_ID) PROFILE_PHOTO
